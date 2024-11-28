dc8ab1d1b0684b31decbe7af3edd459003248fd3
Kooreis <kooreis.official@gmail.com>
2024-11-28 02:29:26 -0600

    The second part of the script adds a line to the main function that calls the Evaluate function with the user's input and prints the result.

--- solution.cs
+++ solution.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-
-public class Program
-{
-    public static void Main()
+public static void Main()
     {
         Console.WriteLine("Enter a ternary expression:");
         string expression = Console.ReadLine();
+        Console.WriteLine("Result: " + Evaluate(expression));
     }
-}
